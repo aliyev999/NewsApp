d8c217f0b5827ba46de743fae6f3e87231f70add
Sabir Aliyev <aliyevv.sabir@gmail.com>
2024-07-01 20:32:41 +0400

    Update

--- NewsApp/About/AboutVC.swift
+++ NewsApp/About/AboutVC.swift
@@ -12,18 +12,5 @@ class AboutVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "About DailyDigest"
-//        navigationController?.navigationBar.prefersLargeTitles = true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
